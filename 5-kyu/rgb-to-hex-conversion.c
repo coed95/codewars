f72002452cae9cbd83e630e5ba7bc6c6ab7b043c
coed95 <codecoed95@gmail.com>
2022-12-20 16:57:50 +0100

    removed stdlib

--- 5-kyu/rgb-to-hex-conversion.c
+++ 5-kyu/rgb-to-hex-conversion.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-#define HEX_LEN 3
+#define RGB_LEN 3
 #define MIN_CLOSEST 0
 #define MAX_CLOSEST 255
 
 int rgb(int r, int g, int b, char *output)
 {
-    char hex_rep[HEX_LEN];
+    char hex_rep[RGB_LEN];
     int i, rgb[] = {r, g, b};
   
     output[0] = '\0';
   
-    for (i = 0; i < 3; ++i)
+    for (i = 0; i < RGB_LEN; ++i)
     {
         if (rgb[i] < MIN_CLOSEST)
           sprintf(hex_rep, "%02X", 0);
